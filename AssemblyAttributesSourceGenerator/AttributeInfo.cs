14c18e468745bc949b3006dd82aa44190ee05f3c
Liza Chevalier <lizalc@pm.me>
2024-02-28 15:20:42 -0600

    feat: assembly attributes source generator
    Implement source generator for providing assembly
    attributes as constants.
    
    Version: 1.0.0

---
+++ AssemblyAttributesSourceGenerator/AttributeInfo.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Collections.Immutable;
+
+namespace AssemblyAttributesSourceGenerator;
+
+public readonly record struct AttributeInfo(string Name, string FullyQualifiedAttributeName)
+{
+    public string ConstructorArgument { get; init; } = "";
+
+    public string RootNamespace { get; init; } = "";
+
+    public string DocSource => $"{FullyQualifiedAttributeName}.{Name}";
+
+    public string ToSourceEntry() => $$"""
+        /// <inheritdoc cref="global::{{DocSource}}"/>
+            internal static string {{Name}} { get; } = "{{ConstructorArgument}}";
+        """;
+}
+
+public readonly record struct AttributeData(string RootNamespace, ImmutableArray<TypedConstant> ConstructorArguments);
+
+public readonly record struct AttributeInfoDetails(string RootNamespace, ImmutableArray<AttributeInfo> AttributeNames);
